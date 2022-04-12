5e5b44f02feb51e7ffc17baee767547ea306dce6
Matt <mgstankov30@gmail.com>
2022-04-12 12:31:23 -0400

    add code adjustments to avoid SQL injection

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -1,3 +1,4 @@
+const query = require('express/lib/middleware/query');
 const { Pool } = require('pg');
 
 const pool = new Pool({
@@ -6,16 +7,19 @@ const pool = new Pool({
   host: 'localhost',
   database: 'bootcampx'
 });
-
-pool.query(`
+const cohortName = process.argv[2];
+const values = [`%${cohortName}%`];
+const queryString = `
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
   FROM assistance_requests
   JOIN teachers ON teachers.id = teacher_id
   JOIN students ON students.id = student_id
   JOIN cohorts ON cohorts.id = cohort_id
-  WHERE cohorts.name LIKE '%${process.argv[2]}%'
+  WHERE cohorts.name LIKE $1
   GROUP BY cohorts.name, teachers.name;
-`)
+`
+
+pool.query(queryString, values)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.cohort}:  ${user.teacher}`);
